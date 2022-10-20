cbee60ee3631dfb0e10e3a3049c1ce3896c96f4d
Lenon <lenonphilippi@gmail.com>
2022-10-20 19:04:27 -0300

    Atualiza-exercícios-pos-desatenção

---
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-01-JavaScript-Primeiros-Passos/ex2.js
@@ -0,0 +1,8 @@
+const notaTecnica = 75;
+if (notaTecnica >= 80){
+    console.log('Parabéns, você foi aprovada(o)!')
+} else if (notaTecnica >=60){
+    console.log('Você está na nossa lista de espera')
+} else {
+    console.log('Você foi reprovada(o)');
+}

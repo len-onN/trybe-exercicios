8a5d4a50491399eb566affe67fbb73cedf75991b
Lenon <lenonphilippi@gmail.com>
2022-10-21 16:11:58 -0300

    exercicio soma array e exercicio media aritmética

--- Fundamentos/seção-04-introdução-à-javascript/Dia-02-JavaScript-Array-e-loop-for/ex1.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-02-JavaScript-Array-e-loop-for/ex1.js
@@ -7,4 +7,22 @@ for (i1 = 0; i1 < numbers.length; i1 = i1 + 1){
     console.log(numbers[i1]);
 }
 
-/**/
+/* - Soma valores do array - */
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];*/
+let soma1 = 0;
+
+for (i2 = 0; i2 < numbers.length; i2 += 1){
+    soma1 = soma1 + numbers[i2];
+    
+}
+console.log(soma1);
+
+/* - Calcula Média Aritimética - */
+
+let med = soma1/numbers.length;
+console.log(med);
+
+/* -  - */
+
+

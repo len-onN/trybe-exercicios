d2ad7be26a16d7b2841ecb95baa4851eddf6cb02
Lenon <lenonphilippi@gmail.com>
2022-10-21 15:55:45 -0300

    mostra arrays concluido

--- Fundamentos/seção-04-introdução-à-javascript/Dia-02-JavaScript-Array-e-loop-for/ex1.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-02-JavaScript-Array-e-loop-for/ex1.js
@@ -1,1 +1,10 @@
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* - Mostrador de valores de arrays - */
+
+for (i1 = 0; i1 < numbers.length; i1 = i1 + 1){
+    console.log(numbers[i1]);
+}
+
+/**/

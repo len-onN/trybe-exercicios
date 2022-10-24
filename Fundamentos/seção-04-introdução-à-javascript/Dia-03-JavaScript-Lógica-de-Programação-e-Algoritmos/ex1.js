4051b9a52ff34f853a8577c4235269a21b7d6aeb
Lenon <lenonphilippi@gmail.com>
2022-10-24 18:44:43 -0300

    exercicio de inverter bananas

--- Fundamentos/seção-04-introdução-à-javascript/Dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/ex1.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/ex1.js
@@ -1,8 +1,17 @@
 /* exercício 1 - fatorial */
 
 let fat = '1';
-for (i = 50; i >= 1; i--){
+for (i = 10; i >= 1; i--){
     fat = fat*i;
 }
 console.log(fat);
 
+/* Estrutura for - inverte palavras */
+
+string = "bananinha";
+gnirts = "";
+
+for (i = string.length - 1; i >= 0 ; i -= 1){
+    gnirts += string[i];
+}
+console.log(gnirts);

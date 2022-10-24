6f96fd0356035f33d78c1da4fbbe668550fec529
Lenon <lenonphilippi@gmail.com>
2022-10-24 19:08:41 -0300

    exercicio maior menor maior e menor

--- Fundamentos/seção-04-introdução-à-javascript/Dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/ex1.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/ex1.js
@@ -1,7 +1,7 @@
 /* exercício 1 - fatorial */
 
 let fat = '1';
-for (i = 10; i >= 1; i--){
+for (let i = 10; i >= 1; i--){
     fat = fat*i;
 }
 console.log(fat);
@@ -11,7 +11,31 @@ console.log(fat);
 string = "bananinha";
 gnirts = "";
 
-for (i = string.length - 1; i >= 0 ; i -= 1){
+for (let i = string.length - 1; i >= 0 ; i -= 1){
     gnirts += string[i];
 }
 console.log(gnirts);
+
+/* Maior string? e Menor String? */
+
+let auray = ['java', 'javascript', 'python4344565', 'html', 'css'];
+let a = auray[0].length;
+let b = '';
+
+for (let i = 0; i < auray.length; i += 1){
+    if (auray[i].length > a ){
+        b = auray[i];
+    }
+}
+console.log(b);
+
+for (let i = 0; i < auray.length; i += 1){
+    if (auray[i].length < a ){
+        b = auray[i];
+    }
+}
+console.log(b);
+
+
+/**/
+

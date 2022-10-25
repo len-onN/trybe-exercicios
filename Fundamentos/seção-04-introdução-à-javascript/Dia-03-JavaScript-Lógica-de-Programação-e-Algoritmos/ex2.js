9b416b48f7f206834900618917db003def10eeb9
Lenon <lenonphilippi@gmail.com>
2022-10-25 19:09:54 -0300

    atualiza-exercicios-bonus

--- Fundamentos/seção-04-introdução-à-javascript/Dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/ex2.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/ex2.js
@@ -1,11 +1,34 @@
 /* - Quadrado de n x n - */
-let n = 3;
-let a = "*";
+let n1 = 3;
+let a1 = "*";
 let b = [0];
 
-for (let i = 0; i < n; i += 1){
-     b[i] = a;  
+for (let i = 0; i < n1; i += 1){
+     b[i] = a1;  
 }
-for (i = 0; i < n; i += 1){
+for (i = 0; i < n1; i += 1){
 console.log(b);
+}
+/* - Exercício piramide - */
+
+let n = 5;
+let a = (n-1)/2;
+let array = [];
+array[a] = "*";
+let s;
+//console.log(array);
+
+for (i = n - 1; i >= 0; i -= 1){
+    if (i != a){
+        array[i] = " ";
+    }
+}
+console.log(array);
+for (o = a; o >= 0; o -= 1){
+    if (o != a){
+    array[o] = "*";
+    s = a - o;
+    array[a + s] = "*";
+    console.log(array);
+}
 }

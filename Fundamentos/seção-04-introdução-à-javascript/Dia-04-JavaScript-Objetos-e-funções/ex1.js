c37f58b4412b91cbca7762f95669e7eaba1aabc5
Lenon <lenonphilippi@gmail.com>
2022-10-26 02:12:16 -0300

    para fixar for in

--- Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex1.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex1.js
@@ -6,3 +6,25 @@ let player = {
 }
 console.log(player['medals']);
 console.log(player.medals.golden);
+
+// Exercício - para fixar - for in e mensagem
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+  for (let i in names){
+    console.log('Olá', names[i]);
+  }
+
+  // Exercício - para fixar - for in - mostra chave e valor
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+  for (let i1 in car){
+    console.log(i1, car[i1]);
+  }

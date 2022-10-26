df1c7947ca7c72b8ae9ceb3640b9a78d54e52eb0
Lenon <lenonphilippi@gmail.com>
2022-10-26 12:49:40 -0300

    exercícios função - 1

---
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex4.js
@@ -0,0 +1,14 @@
+// ex1 
+function isPalindrome (a){
+    let retro = a.split('').reverse().join('');
+    if (retro === a){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// ex2
+function analogue(){
+    
+}

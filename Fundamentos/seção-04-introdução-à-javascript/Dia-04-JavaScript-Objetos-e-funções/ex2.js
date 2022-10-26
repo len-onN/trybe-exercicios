3a91a10e4168a34f586cedb7d3c801911447ef47
Lenon <lenonphilippi@gmail.com>
2022-10-26 11:45:36 -0300

    exercícios - 5 - concatenando valores

--- Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex2.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex2.js
@@ -8,11 +8,30 @@ let info = {
   console.log('Bem-vinda,', info.personagem);
 
 // ex2  
-  info["recorrente"] = 'sim';
+  info["recorrente"] = 'Sim';
   console.log(info);
 
 // ex3
-  for (let i in info){
+for (let i in info){
     console.log(i);
   }
+
 // ex4
+for (let i in info){
+    console.log(info[i]);
+  }
+
+// ex5
+let info1 = {
+    personagem : "Tio Patinhas",
+    origem : "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota : "O último MacPatinhas",
+    recorrente : "Sim",
+};
+for (let i in info){
+    if (i == "recorrente" && info[i] == "Sim" && info1[i] == "Sim") {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[i] + ' e ' + info1[i]);
+    }
+}

7261b64f2fbe46e2515d84f8bf1661f99dd7b542
Lenon <lenonphilippi@gmail.com>
2022-10-26 13:00:28 -0300

    funções - exercício 2

--- Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex4-funcoes.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex4-funcoes.js
@@ -9,6 +9,14 @@ function isPalindrome (a){
 }
 
 // ex2
-function analogue(){
-    
+let array = [2, 3, 6, 7, 10, 1];
+function analogueBIndex(ar){
+    let i = 0;
+    for (let i1 in ar){
+        if (ar[i1] > ar[i]){
+            i = i1;
+        }
+    }
+    return i;
 }
+console.log(analogueBIndex(array));

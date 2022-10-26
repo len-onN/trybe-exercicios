ee2066878ea3b9e01a836739af08de719229a0e4
Lenon <lenonphilippi@gmail.com>
2022-10-26 12:41:52 -0300

    exercício rocket 2 3 - ok

--- Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex3.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex3.js
@@ -10,4 +10,23 @@ let leitor = {
       },
     ],
   };
+  
+  // ex1
   console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo + '.');
+
+  // ex2
+
+  leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      },
+  );
+  console.log(leitor.livrosFavoritos);
+
+  /* ex3 Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte 
+  formato: “Julia tem quantidade livros favoritos”, onde “quantidade” é um número 
+  gerado automaticamente pelo seu código.*/
+  
+  console.log("Julia tem " + leitor.livrosFavoritos.length + " livros favoritos.");

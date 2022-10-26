d5a9b6d6743a4c9701b02dd76ed6f94d3b24078a
Lenon <lenonphilippi@gmail.com>
2022-10-26 15:08:37 -0300

    funções - exercício 4

--- Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex4-funcoes.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex4-funcoes.js
@@ -35,3 +35,23 @@ console.log(bigW (['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 
 // ex4
 let ar1 = [2, 3, 2, 5, 8, 2, 3];
+function rCount (a){ 
+    let c1 = 0;
+    let cr = 0;
+    let idR = 0;
+for (let i in a){
+    let ver = a[i];
+    for (let o in a){
+        if (ver === a[o]){
+            c1 += 1;
+        }
+    }
+    if (c1 > cr){
+        cr = c1;
+        idR = i;
+    }
+    c1 = 0;
+}   
+  return a[idR];
+}
+console.log(rCount([2, 3, 2, 5, 8, 2, 3]));

a0c83ad66e8a31ccd6bb1ec9564d98e69cc2a8a7
Lenon <lenonphilippi@gmail.com>
2022-10-26 11:26:58 -0300

    exercícios - 4 - percorre chaves

--- Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex2.js
+++ Fundamentos/seção-04-introdução-à-javascript/Dia-04-JavaScript-Objetos-e-funções/ex2.js
@@ -1,8 +1,18 @@
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
+// ex1
   console.log('Bem-vinda,', info.personagem);
+
+// ex2  
   info["recorrente"] = 'sim';
   console.log(info);
+
+// ex3
+  for (let i in info){
+    console.log(i);
+  }
+// ex4

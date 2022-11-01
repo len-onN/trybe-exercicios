ac315ee017512f134f743e301e860bc611f91d8f
Lenon <lenonphilippi@gmail.com>
2022-10-31 22:49:28 -0300

    cria e remove

--- Fundamentos/seção-05-javascript-DOM-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/ex2.js
+++ Fundamentos/seção-05-javascript-DOM-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/ex2.js
@@ -1,3 +1,5 @@
+// buscando elemento
+/*
 const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 const pai = elementoOndeVoceEsta.parentElement;
 pai.style.color = 'red';
@@ -7,3 +9,28 @@ const primeiroFilhoDoFilho = pai.firstElementChild;
 const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
 const textElement = elementoOndeVoceEsta.nextSibling;
 const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+*/
+// criando elemento
+
+const pai = document.getElementById('pai'); // Recupere o elemento com o id pai
+const irmaoElementoOndeVoceEsta = document.createElement('section'); // Crie um novo elemento
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const filhoElementoOndeVoceEsta = document.createElement('section');
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+const filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
+filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+/*const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho'); // Recupere o elemento com o id `primeiroFilhoDoFilho`
+const filhoPrimeiroFilhoDoFilho = document.createElement('section'); // Crie um novo elemento 
+filhoPrimeiroFilhoDoFilho.id = 'filhoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);*/
+
+const terceiroFilho = filhoDoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
+console.log(terceiroFilho);

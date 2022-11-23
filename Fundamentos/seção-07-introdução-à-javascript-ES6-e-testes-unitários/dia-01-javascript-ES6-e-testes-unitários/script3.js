f6e1a01e587e65cf6150f5e9af8dc435734a41f8
Lenon <lenonphilippi@gmail.com>
2022-11-23 00:49:20 -0300

    assign

--- Fundamentos/seção-07-introdução-à-javascript-ES6-e-testes-unitários/dia-01-javascript-ES6-e-testes-unitários/script3.js
+++ Fundamentos/seção-07-introdução-à-javascript-ES6-e-testes-unitários/dia-01-javascript-ES6-e-testes-unitários/script3.js
@@ -17,3 +17,36 @@ const lesson1 = {
     professor: 'Maria Clara',
     turno: 'noite',
   };
+
+  /*Crie uma função para adicionar o turno da noite na lesson2. 
+  Essa função deve possuir três parâmetros, sendo eles: 
+  o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.*/
+function newProp (object, newProp, value){
+    object[newProp] = value;
+}
+newProp(lesson2, 'turno', 'noite');
+
+  /*Crie uma função para listar as keys de um objeto. 
+  Essa função deve receber um objeto como parâmetro.*/
+function listKey (object){
+    for (index in object){
+        console.log(index);
+    };
+}
+listKey(lesson2);
+  /*Crie uma função para mostrar o tamanho de um objeto.*/
+function objectLength (object) {
+    console.log(Object.keys(object).length);
+}
+objectLength(lesson2);
+  /*Crie uma função para listar os valores de um objeto. 
+  Essa função deve receber um objeto como parâmetro.*/
+function showValue (object) {
+    console.log(Object.values(object));
+}
+showValue(lesson2);
+    /*Crie um objeto de nome allLessons, que deve 
+    agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, 
+    sendo essas chaves lesson1, lesson2 e lesson3.*/
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);

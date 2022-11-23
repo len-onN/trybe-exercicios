c5acdb6eb660d9a7a6bde0e59c7383c7091a1fd5
Lenon <lenonphilippi@gmail.com>
2022-11-23 10:06:38 -0300

    finaliza primeira parte

--- Fundamentos/seção-07-introdução-à-javascript-ES6-e-testes-unitários/dia-02-javascript-ES6-fluxo-de-exceção-e-objetos/src/script.test.js
+++ Fundamentos/seção-07-introdução-à-javascript-ES6-e-testes-unitários/dia-02-javascript-ES6-fluxo-de-exceção-e-objetos/src/script.test.js
@@ -10,8 +10,12 @@ it('verifica se saída corresponde ao esperado', () => {
 /*Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4];*/
 it('verifica se para certa entrada há uma saída distinta', () => {
     let array = [1, 2, 3, 4];
-    expect(myRemove(array, 3)).not.toBe([1, 2, 3, 4]);
+    expect(myRemove(array, 3)).not.toEqual([1, 2, 3, 4]);
 })
 
-
 /*Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.*/
+it('verifica se entrega o array esperado', () => {
+    let array = [1, 2, 3, 4];
+    myRemove(array, 5)
+    expect(myRemove(array, 5)).toEqual([1, 2, 3, 4]);
+})

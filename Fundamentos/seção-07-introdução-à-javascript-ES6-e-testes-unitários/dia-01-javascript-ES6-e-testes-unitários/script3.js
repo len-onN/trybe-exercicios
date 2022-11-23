4437b23866d9d4fa4188986d51e5185d38914d3f
Lenon <lenonphilippi@gmail.com>
2022-11-23 00:56:36 -0300

    pre push

--- Fundamentos/seção-07-introdução-à-javascript-ES6-e-testes-unitários/dia-01-javascript-ES6-e-testes-unitários/script3.js
+++ Fundamentos/seção-07-introdução-à-javascript-ES6-e-testes-unitários/dia-01-javascript-ES6-e-testes-unitários/script3.js
@@ -50,3 +50,29 @@ showValue(lesson2);
     sendo essas chaves lesson1, lesson2 e lesson3.*/
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 console.log(allLessons);
+
+const getNumberOfStudents = (obj) => {
+  let total = 0;
+
+  const keys = Object.keys(obj);
+    
+  for (index in keys) {
+    total += obj[keys[index]].numeroEstudantes; // incremente a variável total a cada iteração
+  }
+  return total; // a função retorna o total de alunos após as iterações do "for/in"
+};
+console.log(getNumberOfStudents(allLessons));
+
+const getValueByNumber = (obj,number) => Object.values(obj)[number];
+  // usamos o "Object.values()" para criarmos um array com os valores do objeto, e então acessamos a sua posição passando o índice.
+console.log(getValueByNumber(lesson1, 2));
+
+const verifyPair = (obj, key, value) => {
+  const entries = Object.entries(obj);
+  let isEqual = false;
+  for (let index in entries) {
+    if (entries[index][0] === key && entries[index][1] === value) isEqual = true;
+  }
+  return isEqual;
+};
+console.log(verifyPair(lesson2,'professor','Carlos'));

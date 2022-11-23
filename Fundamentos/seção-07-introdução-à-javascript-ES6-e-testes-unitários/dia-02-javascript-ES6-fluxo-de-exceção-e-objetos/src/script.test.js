8a043a75dc1c6b083a3b8cbda82f79b1af681cb3
Lenon <lenonphilippi@gmail.com>
2022-11-23 02:13:15 -0300

    teste 1 ok

---
+++ Fundamentos/seção-07-introdução-à-javascript-ES6-e-testes-unitários/dia-02-javascript-ES6-fluxo-de-exceção-e-objetos/src/script.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('expect');
+const myRemove = require('./script.js');
+/*A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item, caso ele exista no array:
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;*/
+it('verifica se saída corresponde ao esperado', () => {
+    let array = [1, 2, 3, 4];
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);    
+})
+
+/*Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4];*/
+
+/*Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.*/

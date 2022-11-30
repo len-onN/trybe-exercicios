6a082240a2a42ad572a424cd3fefb6c148479ccf
Lenon <lenonphilippi@gmail.com>
2022-11-29 22:11:19 -0300

    exercicio 4-1

--- Fundamentos/seção-08-higher-order-functions-do-javascript-ES6/dia-01-javascript-ES6-introdução-a-higher-order-functions/ex3.js
+++ Fundamentos/seção-08-higher-order-functions-do-javascript-ES6/dia-01-javascript-ES6-introdução-a-higher-order-functions/ex3.js
@@ -1,2 +1,19 @@
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+let cumulative = 0;
+const pointer = (righAns, givenAns) => {
+  righAns.forEach((element, index) => {
+    if (element === givenAns[index]) {
+      cumulative += 1;
+    } else if (givenAns[index] === 'N.A') {        
+    } else {
+      cumulative -= 0.5;
+    }
+})
+return cumulative;
+}
+const noteCounter = (righAns, givenAns, pointer) => {
+    let cumulative = 0;
+    return pointer(righAns, givenAns);
+}
+console.log(noteCounter(RIGHT_ANSWERS, STUDENT_ANSWERS, pointer));
